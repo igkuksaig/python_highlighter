7ada53556b0a4c174c4817e067a500e2d378f516
igku <igkuksaig@gmail.com>
2019-03-24 22:35:21 +0200

    second shot

--- tests/test_highlighter.py
+++ tests/test_highlighter.py
@@ -14,13 +14,17 @@ class HighlightTest(unittest.TestCase):
     def setUp(self):
         """This method is called each time the test routine run"""
         self.app = create_app().test_client()
-        # TODO: add the missing test data in this routine
-        self.highlighted_text = b'Sample <mark>text</mark> to be highlighted'
+
+        self.search_text = 'text'
+        self.text = 'Sample text to be highlighted text'
+        self.highlighted_text = b'Sample <mark>text</mark> to be highlighted <mark>text</mark>'
 
     def tearDown(self):
         """This method is called after the test routine is finished
         to clear out the data created in setUp method."""
-        # TODO: add an implementation
+        self.search_text = ""
+        self.text = ""
+        self.highlighted_text = ""
 
     def test_markup_text(self):
         """Test markup process"""
